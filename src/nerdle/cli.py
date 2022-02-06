8bfe0fe09a25480d33d8a28cccaea98f414bde1d
Alex Cross <alexjcross90@gmail.com>
2022-02-06 11:36:45 +0000

    Add controllers

--- src/nerdle/cli.py
+++ src/nerdle/cli.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser, Namespace
 
+from .controllers import HideController, RunController, SolveController
 from .scoring import Scorer
 from .solver import Solver
 from .views import HideView, RunView, SolveView
@@ -11,24 +12,12 @@ def hide(args: Namespace) -> None:
     size = args.size or len(args.guess)
     best_guess = args.guess or Solver.seed(size)
 
-    view = HideView(size)
     loader = WordLoader(size)
     scorer = Scorer(size)
     solver = Solver(scorer)
-
-    available_answers = loader.common_words
-
-    while True:
-        solutions_by_score = solver.get_possible_solutions_by_score(available_answers, best_guess)
-        highest_score = max(solutions_by_score, key=lambda k: len(solutions_by_score[k]))  # BUG!
-        available_answers = solutions_by_score[highest_score]
-        view.update(best_guess, highest_score, available_answers)
-
-        if scorer.is_perfect_score(highest_score):
-            view.report_success()
-            break
-
-        best_guess = view.get_user_guess()
+    view = HideView(size)
+    controller = HideController(loader, solver, view)
+    controller.hide(best_guess)
 
 
 def solve(args: Namespace) -> None:
@@ -36,24 +25,12 @@ def solve(args: Namespace) -> None:
     size = args.size or len(args.guess)
     best_guess = args.guess or Solver.seed(size)
 
-    view = SolveView(size)
     loader = WordLoader(size)
     scorer = Scorer(size)
     solver = Solver(scorer)
-
-    all_words = loader.all_words
-    available_answers = loader.common_words
-
-    while True:
-        (observed_score, best_guess) = view.get_user_score(best_guess)
-        if scorer.is_perfect_score(observed_score):
-            view.report_success()
-            break
-
-        histogram = solver.get_possible_solutions_by_score(available_answers, best_guess)
-        available_answers = histogram[observed_score]
-        best_guess = solver.get_best_guess(available_answers, all_words)
-        view.report_best_guess(best_guess)
+    view = SolveView(size)
+    controller = SolveController(loader, solver, view)
+    controller.solve(best_guess)
 
 
 def run(args: Namespace) -> None:
@@ -62,23 +39,13 @@ def run(args: Namespace) -> None:
     size = len(solution)
     best_guess = args.guess or Solver.seed(size)
 
-    view = RunView(size)
     loader = WordLoader(size)
     scorer = Scorer(size)
     solver = Solver(scorer)
+    view = RunView(size)
+    controller = RunController(loader, solver, view)
 
-    all_words = loader.all_words
-    available_answers = loader.common_words
-
-    while True:
-        observed_score = scorer.score_word(solution, best_guess)
-        histogram = solver.get_possible_solutions_by_score(available_answers, best_guess)
-        available_answers = histogram[observed_score]
-        view.report_score(solution, best_guess, observed_score, available_answers)
-        if best_guess == solution:
-            break
-
-        best_guess = solver.get_best_guess(available_answers, all_words)
+    controller.run(solution, best_guess)
 
 
 def main() -> None:
---
+++ src/nerdle/controllers.py
@@ -0,0 +1,75 @@
+from .solver import Solver
+from .views import RunView
+from .words import WordLoader
+
+
+class RunController:
+    def __init__(self, loader: WordLoader, solver: Solver, view: RunView) -> None:
+        self.loader = loader
+        self.solver = solver
+        self.view = view
+
+    def run(self, solution: str, best_guess: str) -> None:
+
+        all_words = self.loader.all_words
+        available_answers = self.loader.common_words
+
+        while True:
+            observed_score = self.solver.scorer.score_word(solution, best_guess)
+            histogram = self.solver.get_possible_solutions_by_score(available_answers, best_guess)
+            available_answers = histogram[observed_score]
+            self.view.report_score(solution, best_guess, observed_score, available_answers)
+            if best_guess == solution:
+                break
+
+            best_guess = self.solver.get_best_guess(available_answers, all_words)
+
+
+class SolveController:
+    def __init__(self, loader: WordLoader, solver: Solver, view: RunView) -> None:
+        self.loader = loader
+        self.solver = solver
+        self.view = view
+
+    def solve(self, best_guess: str) -> None:
+
+        all_words = self.loader.all_words
+        available_answers = self.loader.common_words
+
+        while True:
+            (observed_score, best_guess) = self.view.get_user_score(best_guess)
+            if self.solver.scorer.is_perfect_score(observed_score):
+                self.view.report_success()
+                break
+
+            histogram = self.solver.get_possible_solutions_by_score(available_answers, best_guess)
+            available_answers = histogram[observed_score]
+            best_guess = self.solver.get_best_guess(available_answers, all_words)
+            self.view.report_best_guess(best_guess)
+
+
+class HideController:
+    def __init__(self, loader: WordLoader, solver: Solver, view: RunView) -> None:
+        self.loader = loader
+        self.solver = solver
+        self.view = view
+
+    def hide(self, best_guess: str) -> None:
+
+        available_answers = self.loader.common_words
+
+        while True:
+            solutions_by_score = self.solver.get_possible_solutions_by_score(
+                available_answers, best_guess
+            )
+            highest_score = max(
+                solutions_by_score, key=lambda k: len(solutions_by_score[k])
+            )  # BUG!
+            available_answers = solutions_by_score[highest_score]
+            self.view.update(best_guess, highest_score, available_answers)
+
+            if self.solver.scorer.is_perfect_score(highest_score):
+                self.view.report_success()
+                break
+
+            best_guess = self.view.get_user_guess()
