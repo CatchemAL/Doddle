f65ef610ae62ece4722f7c55daf5070d01f28850
Alex Cross <alexjcross90@gmail.com>
2022-02-20 17:47:14 +0000

    Beautiful reporting

--- src/nerdle/view_models.py
+++ src/nerdle/view_models.py
@@ -63,6 +63,11 @@ class ScoreboardPrinter:
         if len(scoreboard) == 1:
             header = self.build_header()
             print(header)
+        elif scoreboard.rows[-1].n > scoreboard.rows[-2].n:
+            is_multi_game_mode = len([row.n for row in scoreboard.rows if row.n == 1]) > 1
+            if is_multi_game_mode:
+                divider = self.build_divider()
+                print(divider)
 
         row = scoreboard.rows[-1]
         row_str_repr = self.build_row(row.n, row.soln, row.guess, row.score, row.num_left)
@@ -83,12 +88,18 @@ class ScoreboardPrinter:
     def build_header(self) -> str:
         repetitions = 0 if self.size <= 5 else (self.size - 5)
         spacing = " " * repetitions
-        dashes = "-" * repetitions
         header = f"\n| # | Soln.{spacing} | Guess{spacing} | Score{spacing} | Poss.{spacing} |\n"
-        header += f"|---|-------{dashes}|-------{dashes}|-------{dashes}|-------{dashes}|"
+        header += self.build_divider()
         return header
 
+    def build_divider(self) -> str:
+        repetitions = 0 if self.size <= 5 else (self.size - 5)
+        dashes = "-" * repetitions
+        return f"|---|-------{dashes}|-------{dashes}|-------{dashes}|-------{dashes}|"
+
     def build_row(self, n: int, soln: Word, guess: Word, score: int, num_left: int) -> str:
+
+        n2 = str(n).rjust(2, " ")
 
         padding = " " * max(0, 5 - self.size)
         padded_score = f"{score}".zfill(self.size)
@@ -99,7 +110,7 @@ class ScoreboardPrinter:
         pretty_guess = self._color_code(guess, score) + padding
         pretty_score = self._color_code(padded_score, score) + padding
 
-        return f"| {n} | {pretty_soln} | {pretty_guess} | {pretty_score} | {padded_num_left} |"
+        return f"|{n2} | {pretty_soln} | {pretty_guess} | {pretty_score} | {padded_num_left} |"
 
     @staticmethod
     def _color_code(word: Word | str, score: int) -> str:
