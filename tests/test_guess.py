361a870e6b810db879230f8d9c85f01f9308efa8
Alex Cross <alexjcross90@gmail.com>
2022-02-27 16:31:15 +0000

    Entropy guess unit tests

--- tests/test_guess.py
+++ tests/test_guess.py
@@ -1,9 +1,9 @@
-from doddle.guess import MinimaxGuess
+from doddle.guess import EntropyGuess, MinimaxGuess
 from doddle.words import Word
 
 
 class TestMinimaxGuess:
-    def test_minimax_where_largest_bucket_differs(self) -> None:
+    def test_minimax_guess_where_largest_bucket_differs(self) -> None:
         # Arrange
         word1 = Word("SNAKE")
         word2 = Word("SHARK")
@@ -18,7 +18,7 @@ class TestMinimaxGuess:
         assert is_better
         assert not is_worse
 
-    def test_minimax_where_common_differs(self) -> None:
+    def test_minimax_guess_where_common_differs(self) -> None:
         # Arrange
         word1 = Word("SNAKE")
         word2 = Word("SHARK")
@@ -33,7 +33,7 @@ class TestMinimaxGuess:
         assert is_better
         assert not is_worse
 
-    def test_minimax_where_number_of_buckets_differ(self) -> None:
+    def test_minimax_guess_where_number_of_buckets_differ(self) -> None:
         # Arrange
         word1 = Word("SNAKE")
         word2 = Word("SHARK")
@@ -48,12 +48,60 @@ class TestMinimaxGuess:
         assert is_better
         assert not is_worse
 
-    def test_minimax_where_all_same_sorts_alphabetically(self) -> None:
+    def test_minimax_guess_where_all_same_sorts_alphabetically(self) -> None:
         # Arrange
         word1 = Word("SNAKE")
         word2 = Word("SHARK")
         guess1 = MinimaxGuess(word1, False, 5, 20)
         guess2 = MinimaxGuess(word2, False, 5, 20)
+
+        # Act
+        is_better = guess1 < guess2
+        is_worse = guess1 > guess2
+
+        # Assert
+        assert not is_better
+        assert is_worse
+
+
+
+class TestEntropyGuess:
+    def test_entropy_guess_where_expected_shannon_entropy_differs(self) -> None:
+        # Arrange
+        word1 = Word("SNAKE")
+        word2 = Word("SHARK")
+        guess1 = EntropyGuess(word1, True, 5.3)
+        guess2 = EntropyGuess(word2, True, 4.8)
+
+        # Act
+        is_better = guess1 < guess2
+        is_worse = guess1 > guess2
+
+        # Assert
+        assert is_better
+        assert not is_worse
+
+    def test_entropy_guess_where_common_differs(self) -> None:
+        # Arrange
+        word1 = Word("SNAKE")
+        word2 = Word("SHARK")
+        guess1 = MinimaxGuess(word1, True, 4, 5.3)
+        guess2 = MinimaxGuess(word2, False, 4, 5.3)
+
+        # Act
+        is_better = guess1 < guess2
+        is_worse = guess1 > guess2
+
+        # Assert
+        assert is_better
+        assert not is_worse
+
+    def test_entropy_guess_where_all_same_sorts_alphabetically(self) -> None:
+        # Arrange
+        word1 = Word("SNAKE")
+        word2 = Word("SHARK")
+        guess1 = MinimaxGuess(word1, False, 5, 5.3)
+        guess2 = MinimaxGuess(word2, False, 5, 5.3)
 
         # Act
         is_better = guess1 < guess2
