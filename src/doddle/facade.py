041a404c7808d99ef8be30e5779137dc793fe349
Alex Cross <alexjcross90@gmail.com>
2022-03-01 08:49:35 +0000

    More experimental reporting

--- src/doddle/facade.py
+++ src/doddle/facade.py
@@ -1,12 +1,14 @@
 from typing import Sequence, TypeVar
+
+from .engine import Engine, SimulEngine
 from .view_models import Scoreboard
 
 from .enums import SolverType
-from .factory import create_engine
+from .factory import create_engine, create_models
 from .views import NullRunView, RunView
 from .words import Word
 
-WordType = TypeVar("WordType", str, Word)
+WordType = str | Word
 
 
 class Doddle:
@@ -15,62 +17,61 @@ class Doddle:
         size: int = 5,
         solver_type: SolverType = SolverType.MINIMAX,
         depth: int = 1,
-        extras: Sequence[Word] | None = None,
+        extras: Sequence[Word] | Sequence[str] | None = None,
         lazy_eval: bool = True,
         reporter: RunView | None = None,
     ):
         self.size = size
+        e = [Word(extra) for extra in extras] if extras else []
 
-        self.engine = create_engine(
+        dictionary, scorer, histogram_builder, solver, simul_solver = create_models(
             size,
             solver_type=solver_type,
             depth=depth,
-            extras=extras,
+            extras=e,
             lazy_eval=lazy_eval,
-            reporter=reporter if reporter else NullRunView(),
         )
 
+        callback = reporter if reporter else NullRunView()
+        self.engine = Engine(dictionary, scorer, histogram_builder, solver, callback)
+        self.simul_engine = SimulEngine(dictionary, scorer, histogram_builder, simul_solver, callback)
+        self.dictionary = dictionary
+
     def __call__(
-        self, answer: WordType, guess: WordType | Sequence[WordType] | None = None
+        self, answer: WordType | Sequence[WordType], guess: WordType | Sequence[WordType] | None = None
     ) -> Scoreboard:
 
-        if answer is None:
-            raise TypeError("Answer cannot be None")
-        if isinstance(answer, WordType.__constraints__):
-            soln = Word(answer)
-        else:
-            message = "The answer but be a valid WordType. Please supply either a string or a word."
-            raise TypeError(message)
+        solns = self.__to_word_list(answer, "answer")
+        guesses = self.__to_word_list(guess, "guess") if guess else []
 
-        if not guess:
-            guesses: list[Word] = []
-        elif isinstance(guess, WordType.__constraints__):
-            guesses = [Word(guess)]
-        else:
-            guesses = [Word(g) for g in guess]
+        size = len(solns[0])
 
-        if len(soln) != self.size:
-            message = f'Expected an answer of size {self.size} but answer, {soln}, is of size {len(soln)}. '
+        missized_solns = [s.value for s in solns if len(s) != self.size]
+        if missized_solns:
+            message = f"All answers must be of length {self.size}: ({', '.join(missized_solns)}). "
             message += "To play Doddle with custom word lengths, please use the size argument when "
             message += "instantiating the Doddle object.\n\n"
-            message += f"e.g.\n    doddle = Doddle(size={len(soln)})"
+            message += f"e.g.\n    doddle = Doddle(size={size})"
             raise ValueError(message)
 
-        missized_words = [g.value for g in guesses if len(g) != self.size]
-        if missized_words:
-            message = f'All guesses must be of size {self.size}: ({", ".join(missized_words)}). '
+        missized_guesses = [g.value for g in guesses if len(g) != self.size]
+        if missized_guesses:
+            message = f'All guesses must be of size {self.size}: ({", ".join(missized_guesses)}). '
             message += "To play Doddle with custom word lengths, please use the size argument when "
             message += "instantiating the Doddle object.\n\n"
-            message += f"e.g.\n    doddle = Doddle(size={len(soln)})"
+            message += f"e.g.\n    doddle = Doddle(size={len(missized_guesses[0])})"
             raise ValueError(message)
-
 
         score_matrix = self.engine.histogram_builder.score_matrix
-        if soln not in score_matrix.potential_solns:
-            message = f"The answer {answer} is not known to Doddle. "
+
+        unknown_solns = [s.value for s in solns if s not in score_matrix.potential_solns]
+        if unknown_solns:
+            missing = ", ".join(unknown_words)
+            missing_extras = "', '".join(unknown_solns)
+            message = f"The following answers are not known to Doddle: {missing}\n"
             message += "To play Doddle with custom words, please use the extras argument when "
             message += "instantiating the Doddle object.\n\n"
-            message += f"e.g.\n    doddle = Doddle(size={len(answer)}, ..., extras=['{answer}'], ...)"
+            message += f"e.g.  doddle = Doddle(size={size}, ..., extras=['{answer}'])"
             raise ValueError(message)
 
         unknown_words = [g.value for g in guesses if g not in score_matrix.all_words]
@@ -80,8 +81,27 @@ class Doddle:
             message = f"The following guesses are not known to Doddle: {missing}\n"
             message += "To play Doddle with custom words, please use the extras argument when "
             message += "instantiating the Doddle object.\n\n"
-            message += f"e.g.\n    doddle = Doddle(size={len(answer)}, ..., extras=['{missing_extras}'], ...)"
+            message += f"e.g.  doddle = Doddle(size={size}, ..., extras=['{missing_extras}'])"
             raise ValueError(message)
 
-        game = self.engine.run(soln, guesses)
-        return game.scoreboard
+        if len(solns) == 1:
+            game = self.engine.run(solns[0], guesses)
+            return game.scoreboard
+
+
+        simul_game = self.simul_engine.run(solns, guesses)
+        return simul_game.scoreboard
+
+
+
+
+    @staticmethod
+    def __to_word_list(words: WordType | Sequence[WordType] | None, label: str) -> list[Word]:
+        if words is None:
+            raise TypeError(f"The {label} cannot be None.")
+
+        if isinstance(words, WordType):
+            soln = Word(words)
+            return [soln]
+
+        return [Word(g) for g in words]
