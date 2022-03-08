98c76707e7f6f35ff58a2701992e547e81831276
Alex Cross <alexjcross90@gmail.com>
2022-03-08 06:27:23 +0000

    histogram tests

--- tests/test_histogram.py
+++ tests/test_histogram.py
@@ -1,5 +1,6 @@
 from doddle.histogram import HistogramBuilder
 from doddle.scoring import Scorer, from_ternary
+from doddle.solver import MinimaxSolver
 from doddle.words import Word, WordSeries
 
 
@@ -21,7 +22,7 @@ class TestHistogramBuilder:
 
         words = [soln for solns in expected.values() for soln in solns]
         potential_solns = WordSeries(words)
-        all_words = WordSeries(words + ["THURL"])
+        all_words = WordSeries(words + [guess.value])
         histogram_builder = HistogramBuilder(Scorer(), all_words, potential_solns)
 
         # Act
@@ -38,3 +39,36 @@ class TestHistogramBuilder:
 
             for soln in bucketed_solns:
                 assert Word(soln) in actual_bucketed_solns
+
+    def test_guess_stream(self) -> None:
+        # Arrange
+        guess = Word("THURL")
+
+        expected = {
+            "00000": ["SNAKE", "SPACE", "SPADE"],
+            "00001": ["SCALE"],
+            "00020": ["SCARE", "SNARE", "SPARE"],
+            "02000": ["SHADE", "SHAKE", "SHAME", "SHAPE", "SHAVE"],
+            "02001": ["SHALE"],
+            "02020": ["SHARE", "SHARK"],
+            "10000": ["SKATE", "STAGE", "STAVE"],
+            "10001": ["SLATE", "STALE"],
+        }
+
+        words = [soln for solns in expected.values() for soln in solns]
+        potential_solns = WordSeries(words)
+        all_words = WordSeries(words + [guess.value])
+        histogram_builder = HistogramBuilder(Scorer(), all_words, potential_solns)
+        guess_factory = MinimaxSolver._create_guess
+
+        # Act
+        guess_stream = histogram_builder.stream(all_words, potential_solns, guess_factory)
+        guesses = list(guess_stream)
+        best_guess = min(guesses)
+
+        # Assert
+        assert best_guess.word == guess
+        assert best_guess.size_of_largest_bucket == len(expected["02000"])
+
+        for g in guesses:
+            assert g.is_common_word ^ (g.word == guess)
